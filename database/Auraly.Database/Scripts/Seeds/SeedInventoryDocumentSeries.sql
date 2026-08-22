43047a71c1a139e812efdda895c17d8c5a8ec078
richard <rjacome@rapidworks.com>
2026-08-22 10:23:27 -0500

    seed commerce document series defaults

--- database/Auraly.Database/Scripts/Seeds/SeedInventoryDocumentSeries.sql
+++ database/Auraly.Database/Scripts/Seeds/SeedInventoryDocumentSeries.sql
@@ -3,7 +3,9 @@ SET ANSI_NULLS ON;
 SET NOCOUNT ON;
 DECLARE @Series TABLE(DocumentType NVARCHAR(64),Prefix NVARCHAR(8));
 INSERT @Series VALUES (N'GoodsReceipt',N'EMC'),(N'StockCount',N'CTI'),
-  (N'InventoryAdjustment',N'AJI'),(N'WarehouseTransfer',N'TRB'),(N'ProductConversion',N'CNV'),(N'Damage',N'AVE');
+  (N'InventoryAdjustment',N'AJI'),(N'WarehouseTransfer',N'TRB'),(N'ProductConversion',N'CNV'),(N'Damage',N'AVE'),
+  (N'SalesReturn',N'DVC'),(N'PurchaseReturn',N'DVP'),
+  (N'ReceivablePayment',N'RCI'),(N'PayablePayment',N'EGR');
 INSERT dbo.DocumentSeries(DocumentSeriesId,BusinessId,DeviceId,DocumentType,Prefix,SeriesCode,Padding,RangeStart,RangeEnd,IsOfflineCapable,IsActive,CreatedAt)
 SELECT NEWID(),b.BusinessId,NULL,s.DocumentType,s.Prefix,N'00',8,1,99999999,0,1,SYSDATETIMEOFFSET()
 FROM dbo.Businesses b CROSS JOIN @Series s
